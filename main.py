6029f501ec4f6a32e8b9475a19d9cb1d64bf8d95
Güney Can Yılmaz <160347174+GCanYil@users.noreply.github.com>
2025-11-21 01:08:52 +0300

    Implement bulk generation loop and dynamic text alignment
    Integrated Pandas for Excel data extraction. Implemented a loop to process each participant row. Added dynamic X-coordinate calculation logic using Pillow's textbbox to center-align names and dates on the certificate template

--- main.py
+++ main.py
@@ -9,29 +9,30 @@ name_font = ImageFont.truetype("assets/fonts/Mea_Culpa/MeaCulpa-Regular.ttf", 15
 description_font = ImageFont.truetype("assets/fonts/Roboto/Roboto-VariableFont_wdth,wght.ttf", 75)
 date_font = ImageFont.truetype("assets/fonts/Roboto/Roboto-VariableFont_wdth,wght.ttf", 30)
 
-img = Image.open("assets/certificate-templates/template1.png")
-width = img.width
+for row in rows_to_list:
+    name = row[0]
+    type_of = "of achievement"
+    certificate_date = row[1]
+    email = row[2]
 
-name = "Dünyanın en güzel kızı Mercan"
-type_of = "of appreciation"
-certificate_date = "Date: 23.11.2025"
-draw = ImageDraw.Draw(img)
+    img = Image.open("assets/certificate-templates/template1.png")
+    width = img.width
+    draw = ImageDraw.Draw(img)
 
-box = draw.textbbox((0,0), name, font=name_font)
-type_box = draw.textbbox((0,0), type_of, font=description_font)
-date_box = draw.textbbox((0,0), certificate_date, font=date_font)
+    box = draw.textbbox((0,0), name, font=name_font)
+    type_box = draw.textbbox((0,0), type_of, font=description_font)
+    date_box = draw.textbbox((0,0), certificate_date, font=date_font)
 
-box_width = box[2] - box[0]
-type_box_width = type_box[2] - type_box[0]
-date_box_width = date_box[2] - date_box[0]
+    box_width = box[2] - box[0]
+    type_box_width = type_box[2] - type_box[0]
+    date_box_width = date_box[2] - date_box[0]
 
-x_coord = int((width - box_width) /2)
-type_box_x = int((width - type_box_width) /2)
-date_box_x = int((3400 - date_box_width) /2)
+    x_coord = int((width - box_width) /2)
+    type_box_x = int((width - type_box_width) /2)
+    date_box_x = int((3400 - date_box_width) /2)
 
-draw.text((x_coord,600), name, font=name_font, fill=(0,0,0))
-draw.text((type_box_x,350), type_of, font=description_font, fill=(0,0,0))
-draw.text((date_box_x,1350), certificate_date, font=date_font, fill=(0,0,0))
+    draw.text((x_coord,600), name, font=name_font, fill=(0,0,0))
+    draw.text((type_box_x,350), type_of, font=description_font, fill=(0,0,0))
+    draw.text((date_box_x,1350), certificate_date, font=date_font, fill=(0,0,0))
 
-img.show()
-
+    img.save(f"output/{name}.png")
