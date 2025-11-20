83353876f7278e0b2027c46d739cd4c86daefae7
Güney Can Yılmaz <160347174+GCanYil@users.noreply.github.com>
2025-11-21 01:00:21 +0300

    Implement dynamic text alignment logic with Pillow

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
 import pandas as pd
+from PIL import Image, ImageDraw, ImageFont
 
 df = pd.read_excel("data/veri.xlsx")
-df["Tarih"] = df["Tarih"].dt.strftime("%d.%m.%y")
+df["Tarih"] = df["Tarih"].dt.strftime("%d.%m.%Y")
 rows_to_list = df.values.tolist()
+
+name_font = ImageFont.truetype("assets/fonts/Mea_Culpa/MeaCulpa-Regular.ttf", 150)
+description_font = ImageFont.truetype("assets/fonts/Roboto/Roboto-VariableFont_wdth,wght.ttf", 75)
+date_font = ImageFont.truetype("assets/fonts/Roboto/Roboto-VariableFont_wdth,wght.ttf", 30)
+
+img = Image.open("assets/certificate-templates/template1.png")
+width = img.width
+
+name = "Dünyanın en güzel kızı Mercan"
+type_of = "of appreciation"
+certificate_date = "Date: 23.11.2025"
+draw = ImageDraw.Draw(img)
+
+box = draw.textbbox((0,0), name, font=name_font)
+type_box = draw.textbbox((0,0), type_of, font=description_font)
+date_box = draw.textbbox((0,0), certificate_date, font=date_font)
+
+box_width = box[2] - box[0]
+type_box_width = type_box[2] - type_box[0]
+date_box_width = date_box[2] - date_box[0]
+
+x_coord = int((width - box_width) /2)
+type_box_x = int((width - type_box_width) /2)
+date_box_x = int((3400 - date_box_width) /2)
+
+draw.text((x_coord,600), name, font=name_font, fill=(0,0,0))
+draw.text((type_box_x,350), type_of, font=description_font, fill=(0,0,0))
+draw.text((date_box_x,1350), certificate_date, font=date_font, fill=(0,0,0))
+
+img.show()
 
